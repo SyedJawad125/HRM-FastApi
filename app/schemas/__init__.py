ef29a0c7b6b6446db471b9398f86cfb324aded8a
Syed Jawad Ali <syedjawadali92@gmail.com>
2025-07-04 17:12:18 +0500

    Changes

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,38 +1,77 @@
-from .user import LoginRequest, Token
+# from .user import LoginRequest, Token
+# from .user import (
+#     UserBase,
+#     UserCreate,
+#     LoginRequest,  # Make sure this is included
+#     UserUpdate,
+#     UserOut,
+#     Token,
+#     TokenData
+# )
+# from .employee import Employee, EmployeeCreate
+# from .department import Department, DepartmentCreate
+# from .user import UserOut, UserCreate, UserUpdate, Token, TokenData
+# from .user import LoginRequest, Token
+# __all__ = [
+#     'UserBase', 'UserCreate', 'LoginRequest', 'UserUpdate', 'UserOut',
+#     'Token', 'TokenData',
+#     'Employee', 'EmployeeCreate',
+#     'Department', 'DepartmentCreate'
+# ]
+
+# from .department import (
+#     DepartmentBase,
+#     DepartmentCreate,
+#     Department,
+#     PaginatedDepartments,
+#     DepartmentListResponse,
+#     DepartmentUpdate
+# )
+
+# __all__ = [
+#     'DepartmentBase',
+#     'DepartmentCreate',
+#     'Department',
+#     'PaginatedDepartments',
+#     'DepartmentListResponse',
+#     'DepartmentUpdate'
+# ]
+
+
+
+
+# user schemas
 from .user import (
     UserBase,
     UserCreate,
-    LoginRequest,  # Make sure this is included
+    LoginRequest,
     UserUpdate,
     UserOut,
     Token,
     TokenData
 )
-from .employee import Employee, EmployeeCreate
-from .department import Department, DepartmentCreate
-from .user import UserOut, UserCreate, UserUpdate, Token, TokenData
-from .user import LoginRequest, Token
-__all__ = [
-    'UserBase', 'UserCreate', 'LoginRequest', 'UserUpdate', 'UserOut',
-    'Token', 'TokenData',
-    'Employee', 'EmployeeCreate',
-    'Department', 'DepartmentCreate'
-]
 
+# employee schemas
+from .employee import (
+    Employee,
+    EmployeeCreate
+)
+
+# department schemas
 from .department import (
     DepartmentBase,
     DepartmentCreate,
     Department,
+    DepartmentUpdate,
     PaginatedDepartments,
-    DepartmentListResponse,
-    DepartmentUpdate
+    DepartmentListResponse
 )
 
+# define what will be exported on `from schemas import *`
 __all__ = [
-    'DepartmentBase',
-    'DepartmentCreate',
-    'Department',
-    'PaginatedDepartments',
-    'DepartmentListResponse',
-    'DepartmentUpdate'
+    'UserBase', 'UserCreate', 'LoginRequest', 'UserUpdate', 'UserOut',
+    'Token', 'TokenData',
+    'Employee', 'EmployeeCreate',
+    'DepartmentBase', 'DepartmentCreate', 'Department', 'DepartmentUpdate',
+    'PaginatedDepartments', 'DepartmentListResponse'
 ]
