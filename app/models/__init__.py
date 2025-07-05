743f9036a35f0bc450c5e20e357ed89eb33c15c4
Syed Jawad Ali <syedjawadali92@gmail.com>
2025-07-05 19:08:19 +0500

    Changes

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,7 +2,10 @@ from .user import User
 from .employee import Employee
 from .department import Department
 from .permission import Permission
-from .role import Role
+from .role import Role, role_permission  # bring role_permission here
+from .association_tables import user_role  # bring user_role from new file
 
-
-__all__ = ["User", "Employee", "Department", "Permission", "Role"]
+__all__ = [
+    "User", "Employee", "Department", "Permission",
+    "Role", "role_permission", "user_role"
+]
