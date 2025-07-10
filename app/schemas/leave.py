93b01dedac420ead06f5db0da7bcdb01ba3bfe74
Syed Jawad Ali <syedjawadali92@gmail.com>
2025-07-11 00:24:11 +0500

    Changes

--- app/schemas/leave.py
+++ app/schemas/leave.py
@@ -1,82 +1,3 @@
-# from pydantic import BaseModel, EmailStr
-# from datetime import datetime
-# from typing import Optional
-# from enum import Enum
-
-# class LeaveStatus(str, Enum):
-#     PENDING = "pending"
-#     APPROVED = "approved"
-#     REJECTED = "rejected"
-
-# class LeaveType(str, Enum):
-#     ANNUAL = "annual"
-#     SICK = "sick"
-#     MATERNITY = "maternity"
-#     PATERNITY = "paternity"
-#     UNPAID = "unpaid"
-#     OTHER = "other"
-
-# class LeaveBase(BaseModel):
-#     start_date: datetime
-#     end_date: datetime
-#     leave_type: LeaveType
-#     reason: str
-
-# class LeaveCreate(LeaveBase):
-#     pass
-
-# class LeaveUpdate(BaseModel):
-#     status: LeaveStatus
-#     approved_by_id: Optional[int] = None
-
-
-# class Approver(BaseModel):
-#     id: int
-#     name: str
-
-#     class Config:
-#         from_attributes = True
-# class LeaveResponse(LeaveBase):
-#     id: int
-#     status: LeaveStatus
-#     created_at: datetime
-#     updated_at: Optional[datetime] = None
-#     employee_id: int
-#     approved_by_id: Optional[int] = None
-#     approved_by: Optional[Approver] = None  # Nested object
-
-#     class Config:
-#         from_attributes = True
-
-# class LeaveList(BaseModel):
-#     id: int
-#     start_date: datetime
-#     end_date: datetime
-#     leave_type: LeaveType
-#     status: LeaveStatus
-#     employee_id: int
-
-#     class Config:
-#         from_attributes = True 
-
-# class LeaveListResponse(BaseModel):
-#     count: int
-#     data: list[LeaveList]
-
-
-# class LeaveListResult(BaseModel):
-#     count: int
-#     data: list[LeaveList]
-
-#     class Config:
-#         from_attributes = True
-
-# class MyLeaveListResponse(BaseModel):
-#     status: str
-#     result: LeaveListResult
-
-
-
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
@@ -107,8 +28,6 @@ class LeaveCreate(LeaveBase):
 class LeaveUpdate(BaseModel):
     status: LeaveStatus
     approved_by_id: Optional[int] = None
-
-
 
 
 class CreateLeaveResponse(LeaveBase):
@@ -149,23 +68,6 @@ class LeaveResponse(LeaveBase):
         from_attributes = True
 
 
-# ✅ Add approved_by_id in this schema
-# class LeaveList(BaseModel):
-#     id: int
-#     start_date: datetime
-#     end_date: datetime
-#     leave_type: LeaveType
-#     status: LeaveStatus
-#     employee_id: int
-#     approved_by_id: Optional[int] = None  # ✅ Added line
-
-#     class Config:
-#         from_attributes = True
-
-
-# class LeaveListResponse(BaseModel):
-#     count: int
-#     data: list[LeaveList]
 class Role(BaseModel):
     id: int
     name: str  # adjust this field based on your actual Role model
